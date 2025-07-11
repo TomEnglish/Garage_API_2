1582cc7097a6d4a23defb42926339c9ea98858db
tomenglish <t.elliott.english@gmail.com>
2025-07-11 14:08:16 -0400

    updated ports for Render

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,18 @@
+import os
 from app import create_app
 from app.models import db
 
 #app = create_app("DevelopmentConfig")
 app = create_app('ProductionConfig')
 
-
 with app.app_context():
     #db.drop_all()
     db.create_all()
 
-app.run()
+# Fix for Render deployment: bind to 0.0.0.0 and use PORT environment variable
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
 
 
